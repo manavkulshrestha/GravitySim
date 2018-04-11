c7258fc9846afde7087ee7bfb25e8d9479a8c702
manavkulshrestha <manavkoolz@gmail.com>
2018-04-11 15:26:01 -0400

    Started work on Body class

---
+++ src/Body.java
@@ -0,0 +1,120 @@
+/*
+    Manav Kulshrestha
+    Body.java
+    4/9/18
+*/
+
+public class Body {
+    private final double G = .00000000006674;//TODO probably use a much larger G
+    private String label;
+    private double xPosition;
+    private double yPosition;
+    private double iVector;
+    private double jVector;
+    private double mass;
+    private double radius;
+
+    public Body(String label, double m, double r, double x, double y, double i, double j) {
+        this.label = label;
+        this.xPosition = x;
+        this.yPosition = y;
+        this.iVector = i;
+        this.jVector = j;
+        this.mass = m;
+        this.radius = r;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    public void setLabel(String newLabel) {
+        this.label = newLabel;
+    }
+
+    public double getXPosition() {
+        return this.xPosition;
+    }
+
+    public void setXPosition(double newXPosition) {
+        this.xPosition = newXPosition;
+    }
+
+    public double getYPosition() {
+        return this.yPosition;
+    }
+
+    public void setYPosition(double newYPosition) {
+        this.xPosition = newYPosition;
+    }
+
+    public double getIVector() {
+        return this.iVector;
+    }
+
+    public void setIVector(double newIVector) {
+        this.iVector = newIVector;
+    }
+
+    public double getJVector() {
+        return this.jVector;
+    }
+
+    public void setJVector(double newJVector) {
+        this.jVector = newJVector;
+    }
+
+    public double getMass() {
+        return this.mass;
+    }
+
+    public void setMass(double newMass) {
+        this.mass = mass;
+    }
+
+    public Body copy() {
+        return new Body(this.label, this.mass, this.radius, this.xPosition, this.yPosition, this.iVector, this.jVector);
+    }
+
+    public double getAbsoluteDistance(Body body) {
+        return Math.sqrt(Math.pow(this.getXDistance(body), 2) + Math.pow(this.getYDistance(body), 2));
+    }
+
+    public double getXDistance(Body body) {
+        return this.xPosition-body.getXPosition();
+    }
+
+    public double getYDistance(Body body) {
+        return this.yPosition-body.getYPosition();
+    }
+
+    public void accelerateByGravity(Body body) {
+        double h = this.getAbsoluteDistance(body), x = this.getXDistance(body), y = this.getYDistance(body);
+        double a = G*body.getMass()/Math.pow(h, 2);
+
+        double sin = y/h;
+        double cos = x/h;
+
+        if(x == 0) {
+            this.jVector += a;
+        } else if(y == 0) {
+            this.iVector += a;//            ., or ,.
+        } else if(x > 0) {//body this
+            if(y > 0) {//this|body
+                this.iVector += -a*cos;//       .
+                this.jVector += -a*sin;//   ,
+            } else if(y < 0) {//body|this
+                this.iVector += -a*cos;//   ,
+                this.jVector += a*sin;//        .
+            }
+        } else if(x < 0) {//this body
+            if(y > 0) {//this|body
+                this.iVector += a*cos;//    .
+                this.jVector += -a*sin;//       ,
+            } else if(y < 0) {//body|this
+                this.iVector += a*cos;//        ,
+                this.jVector += a*sin;//    .
+            }
+        }
+    }
+}
---
+++ src/System.java
@@ -0,0 +1,18 @@
+/*
+    Manav Kulshrestha
+    System.java
+    4/9/18
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class System {
+    public static void main(String args[]) {
+        List<Body> bodies = new ArrayList<>();
+
+
+    }
+
+
+}
